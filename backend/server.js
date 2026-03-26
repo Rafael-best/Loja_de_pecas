964668d6584ba26290044b85c2951dc7b334e0c2
Rafael-best <rafaelfrozza100@gmail.com>
2026-03-26 01:01:19 -0300

    atualização cod erro banco de dados

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-
-const routes = require("./routes");
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
-app.get("/", (req,res)=>{
-    res.send("API funcionando");
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
 });
