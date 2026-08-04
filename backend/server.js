6eb8d4844941fa3b2edfacd491dd689703aa6f02
Rafael-best <rafaelfrozza100@gmail.com>
2026-08-04 20:43:15 -0300

    aula 0408

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
+const routes = require("./routes");
+
 const app = express();
 
-
-
 app.use(cors());
-
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use("/api", routes);
 
-
-const loginRoutes = require("./routes/login");
-
-app.use("/api/login", loginRoutes);
-
-
-
-app.get("/", (req, res) => {
-    res.json({
-        sistema: "Loja de Peças",
-        versao: "2.0",
-        status: "Servidor Online",
-        data: new Date()
-    });
+app.get("/", (req,res)=>{
+    res.send("API funcionando");
 });
-
-
-
-app.use((err, req, res, next) => {
-
-    console.error(err.stack);
-
-    res.status(500).json({
-        sucesso: false,
-        mensagem: "Erro interno do servidor."
-    });
-
-});
-
-
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-
-    console.log("======================================");
-    console.log("🚗 LOJA DE PEÇAS");
-    console.log("======================================");
-    console.log(`Servidor: http://localhost:${PORT}`);
-    console.log("API: http://localhost:3000/api");
-    console.log("======================================");
-
+    console.log("Servidor rodando em http://localhost:3000");
 });
